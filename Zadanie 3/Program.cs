516f84ccece7386d453c98ae6d4e0d8df04cd9ff
xchomiczo <xchomiczo@gmail.com>
2024-03-15 12:34:52 +0100

    Add project files.

---
+++ Zadanie 3/Program.cs
@@ -0,0 +1,118 @@
+﻿using GLFW;
+using GlmSharp;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+using System.Drawing;
+
+using Shaders;
+
+namespace PMLabs
+{
+
+
+    //Implementacja interfejsu dostosowującego metodę biblioteki Glfw służącą do pozyskiwania adresów funkcji i procedur OpenGL do współpracy z OpenTK.
+    public class BC : IBindingsContext
+    {
+        public IntPtr GetProcAddress(string procName)
+        {
+            return Glfw.GetProcAddress(procName);
+        }
+    }
+
+    class Program
+    {
+        static ShaderProgram shader;
+
+        static float speed_y; //Prędkość obrotu wokół osi Y [rad/s]
+        static float speed_x; //Prędkość obrotu wokół osi X [rad/s]
+
+        static KeyCallback kc = KeyProcessor;
+
+        public static void KeyProcessor(System.IntPtr window, Keys key, int scanCode, InputState state, ModifierKeys mods)
+        {
+            if (state == InputState.Press)
+            {
+                if (key == Keys.Left) speed_y = -3.14f;
+                if (key == Keys.Right) speed_y = 3.14f;
+                if (key == Keys.Up) speed_x = -3.14f;
+                if (key == Keys.Down) speed_x = 3.14f;
+            }
+            if (state == InputState.Release)
+            {
+                if (key == Keys.Left) speed_y = 0;
+                if (key == Keys.Right) speed_y = 0;
+                if (key == Keys.Up) speed_x = 0;
+                if (key == Keys.Down) speed_x = 0;
+            }
+        }
+
+        public static void InitOpenGLProgram(Window window)
+        {
+            GL.ClearColor(0, 0, 0, 1);
+            shader = new ShaderProgram("v_shader.glsl", "f_shader.glsl");
+            Glfw.SetKeyCallback(window, kc);
+        }
+
+        public static void FreeOpenGLProgram(Window window)
+        {
+
+        }
+
+        public static void DrawScene(Window window, float angle_x, float angle_y)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            mat4 P = mat4.Perspective(glm.Radians(50.0f), 1, 1, 50);
+            mat4 V = mat4.LookAt(new vec3(0, 0, -5), new vec3(0, 0, 0), new vec3(0, 1, 0));
+
+            shader.Use();
+            GL.UniformMatrix4(shader.U("P"), 1, false, P.Values1D);
+            GL.UniformMatrix4(shader.U("V"), 1, false, V.Values1D);
+
+            mat4 M = mat4.Rotate(angle_y, new vec3(0, 1, 0)) * mat4.Rotate(angle_x, new vec3(1, 0, 0));
+            GL.UniformMatrix4(shader.U("M"), 1, false, M.Values1D);
+
+
+
+            Glfw.SwapBuffers(window);
+        }
+
+        static void Main(string[] args)
+        {
+            Glfw.Init();
+
+            Window window = Glfw.CreateWindow(500, 500, "Programowanie multimedialne", GLFW.Monitor.None, Window.None);
+
+            Glfw.MakeContextCurrent(window);
+            Glfw.SwapInterval(1);
+
+            GL.LoadBindings(new BC());
+
+            InitOpenGLProgram(window);
+
+            float angle_x = 0;
+            float angle_y = 0;
+
+            Glfw.Time = 0;
+
+            while (!Glfw.WindowShouldClose(window))
+            {
+                angle_x += speed_x * (float)Glfw.Time;
+                angle_y += speed_y * (float)Glfw.Time;
+                Glfw.Time = 0;
+                DrawScene(window, angle_x, angle_y);
+
+                Glfw.PollEvents();
+            }
+
+
+            FreeOpenGLProgram(window);
+
+            Glfw.Terminate();
+        }
+
+
+    }
+}
